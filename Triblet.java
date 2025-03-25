ac4ec8ddbbc4093c28998f8cc06ae38818358f6a
Ramvarsha R <ramvarsha2710@gmail.com>
2025-03-25 21:56:03 +0530

    Triblet.java

---
+++ Triblet.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int ar[]=new int[n];
+		for(int i=0;i<n;i++){
+		    ar[i]=in.nextInt();
+		}
+		int x=ar[0], y=ar[1];
+		for(int i=2;i<=n;i++){
+		    int z=x+y;
+		    if(z==i){
+		        count++;
+		    }
+		    x=y;
+		    y=i;
+		}
+		System.out.print(count);
+	}
+}
