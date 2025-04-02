3bbe6a9e8eb4a5d1b64367de927adaad8cfb133b
Ramvarsha R <ramvarsha2710@gmail.com>
2025-04-02 10:34:16 +0530

    Update CountCo-primePairs.java

--- CountCo-primePairs.java
+++ CountCo-primePairs.java
@@ -8,20 +8,18 @@ class Main
 		for(int i=0;i<n;i++){
 		    ar[i]=in.nextInt();
 		}
-		int a=0, b=0;
 		int count=0;
-		for(int i=1;i<=n;i++){
-		    for(int j=i+1;j<=n;j++){
-		        a=i;
-		        b=j;
+		for(int i=0;i<n;i++){
+		    for(int j=i+1;j<n;j++){
+		        int a=ar[i], b=ar[j];
 		        while(b!=0){
 		            int temp=b;
 		            b=a%b;
 		            a=temp;
 		        }
-		    }
-		    if(a==1){
-		        count++;
+			if(a==1){
+				count++;
+			}
 		    }
 		}
 		System.out.print(count);
