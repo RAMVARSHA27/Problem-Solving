07b7f930c1a4a5820d1c75b7ddd5e3f2af80efd6
Ramvarsha R <ramvarsha2710@gmail.com>
2025-04-14 16:26:34 +0530

    SplitAStringInBalancedStrings.java

--- SplitAStringInBalancedStrings.java
+++ SplitAStringInBalancedStrings.java
@@ -4,19 +4,16 @@ class Main
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		String s=in.nextLine();
-		int r=0, l=0, c=0;
-		for(int i=0,j=0;j<s.length();i++){
-		    if(s.charAt(j)=='R'){
+		int r=0,l=0,c=0;
+		for(int i=0;i<s.length();i++){
+		    if(s.charAt(i)=='R'){
 		        r++;
-		        j++;
 		    }
 		    else{
 		        l++;
-		        j++;
 		    }
 		    if(r==l){
 		        c++;
-		        i=j;
 		        r=0;
 		        l=0;
 		    }
