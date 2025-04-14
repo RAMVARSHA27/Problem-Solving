96354f4f408cc3a4f99ecd35d8523652b25c608b
Ramvarsha R <ramvarsha2710@gmail.com>
2025-04-14 11:21:59 +0530

    CountNumberOfDistinctIntegersAfterReverseOperations.java

--- CountNumberOfDistinctIntegersAfterReverseOperations.java
+++ CountNumberOfDistinctIntegersAfterReverseOperations.java
@@ -3,23 +3,33 @@ class Main
 {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
-		int n=in.nextInt();
-		int ar[]=new int[n];
-		for(int i=0;i<n;i++){
-		    ar[i]=in.nextInt();
-		}
-		HashSet<Integer> set=new HashSet<>();
-		for(int num:ar){
-		    set.add(num);
-		    set.add(reverse(num));
-		}
-		System.out.print(set.size());
+		List<Integer> li=new ArrayList<>();
+	    while(true){
+	        int n=in.nextInt();
+	        if(n==-1){
+	            break;
+	        }
+	        li.add(n);
+	    }
+	    int ar[]=new int[li.size()];
+	    for(int i=0;i<li.size();i++){
+	        ar[i]=li.get(i);
+	    }
+	    Set<Integer>set=new HashSet<>();
+	    for(int num:ar){
+	        set.add(num);
+	    }
+	    for(int num:ar){
+	        int rev=revNumber(num);
+	        set.add(rev);
+	    }
+	    System.out.print(set.size());
 	}
-	static int reverse(int no){
+	private static int revNumber(int num){
 	    int rev=0;
-	    while(no>0){
-	        rev=rev*10+(no%10);
-	        no/=10;
+	    while(num>0){
+	        rev=rev*10+num%10;
+	        num/=10;
 	    }
 	    return rev;
 	}
