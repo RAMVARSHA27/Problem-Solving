ec002dfc8238ccce9e6f9e5f49a6b4e227ccbf63
RAMVARSHA R <ramvarsha2710@gmail.com>
2025-07-10 17:54:14 +0530

    Update Triblet.java

--- Triblet.java
+++ Triblet.java
@@ -4,6 +4,7 @@ class Main
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		int n=in.nextInt();
+		int count=0;
 		int ar[]=new int[n];
 		for(int i=0;i<n;i++){
 		    ar[i]=in.nextInt();
